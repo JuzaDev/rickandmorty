985cb3b9ea5082bc0d3c4b72df0f094ba9c5f16a
JuzaDev <paolo.itri89@gmail.com>
2024-11-25 16:34:45 +0100

    refactor dev code

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,11 +1,5 @@
 import { Outlet } from "react-router-dom";
 
 export default function RootLayout(): JSX.Element {
-  return (
-    <>
-      {/* <main> */}
-      <Outlet />
-      {/* </main> */}
-    </>
-  );
+  return <Outlet />;
 }
